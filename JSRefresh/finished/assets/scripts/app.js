e52a09780e6aeb3ccd634b819ce4e7882367a030
Jaehoon Park <wido1593@gmail.com>
2023-10-25 14:32:56 +0900

    리엑트 스터디 - JS 핵심 기능 복습 [import/export]

---
+++ JSRefresh/finished/assets/scripts/app.js
@@ -0,0 +1,18 @@
+// export한 변수를 import { 변수명 } from "(파일명)"으로 읽어온다.
+//import { apiKey } from "./util.js";
+
+//파일당 하나의 값으로 지정되는 default는 아래와 같이 이름과 파일을 지정한다.
+//import apiKey from "./util.js";
+
+//{,} 안에 ',' 로 구분하여 여러개의 변수를 가져올 수 있다.
+// as 를 사용하여 다른 이름으로 사용 가능.
+import {apiKey, abc as content} from "./util.js";
+
+console.log(apiKey);
+console.log(content);
+
+// 그룹으로 가져올 수 있다.
+import * as util from "./util.js";
+
+console.log(util.apiKey); //. 접근자로 그룹 import 변수 읽어오기
+
---
+++ JSRefresh/finished/assets/scripts/util.js
@@ -0,0 +1,14 @@
+
+// String 변수를 선언하고 이를 다른 파일에서 사용할 수 있도록 export
+//export let apiKey = "vavasjlsjks"
+
+//파일에 하나만 지정할 수 있는 default 키워드로 export 할 수 있다.
+//export default "vavasjlsjks"
+
+//default 변수와, 명명 변수는 함께 사용할 수 있다.
+export default "vavasjlsjks"
+export let apiKey = "vavasjlsjks"
+export let abc = "abc";
+
+
+
