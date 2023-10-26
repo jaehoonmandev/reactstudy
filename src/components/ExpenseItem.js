dcca9da902e7014bc10484799db594a48b85e0d3
Jaehoon Park <wido1593@gmail.com>
2023-10-26 18:52:12 +0900

    React.js 학습기록 - props와 props.children

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import './ExpenseItem.css'
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 function ExpenseItem(
     // 하나하나 키 값을 주고 받아 왔을 떄 props.date 와 같이 사용하는 것이 아닌
     // 디스트럭처링을 통하여 바로 키 호출로 값을 불러올 수 있다.
@@ -12,14 +13,16 @@ function ExpenseItem(
             {/*<div>{date.toISOString()}</div>*/}
             {/*<div>{props.expense.date.toISOString()}</div>*/}
             {/*컴포넌트화 한 ExpenseDate로 props를 넘겨주고 JSX 를 return 받는다.*/}
-            <ExpenseDate date={props.expense.date}></ExpenseDate>
 
-            <div className="expense-item__description">
+
+            {/*부모 컴포넌트안에 자식 컴포넌트를 지정한다.*/}
+            <Card className="expense-item__description">
                 {/*<h2>{title}</h2>*/}
+                <ExpenseDate date={props.expense.date}></ExpenseDate>
                 <h2>{props.expense.title}</h2>
                 {/*<div className="expense-item__price">${amount}</div>*/}
                 <div className="expense-item__price">${props.expense.amount}</div>
-            </div>
+            </Card>
         </div>
     );
 }
