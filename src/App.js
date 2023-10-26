6457e84af3acb166c783ac1c6a0ac806fa568a4e
Jaehoon Park <wido1593@gmail.com>
2023-10-26 17:07:36 +0900

    React.js 학습기록 - props로 값 넘겨주기(1)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+    const expenses = [
+        {
+            id: 'e1',
+            title:'Toliet Paper' ,
+            amount: 94.12,
+            date: new Date(2023, 10,25)
+        },
+        {
+            id: 'e2',
+            title:'Car Insurance' ,
+            amount: 294.67,
+            date: new Date(2023, 10,26)
+        },
+        {
+            id: 'e3',
+            title:'Coffee' ,
+            amount: 7.8,
+            date: new Date(2023, 10,26)
+        }
+
+    ];
+
     // 기존의 JS는 document.createElement/getElementById 와 같이 지정하지만.
     // 리액트는 이 작업을 직접 지정하지 않는다.
   return (
     <div>
         <h2>Let's get started!</h2>
         <p>This is also visible!</p>
+        <ExpenseItem
+            title={expenses[0].title}
+            amount={expenses[0].amount}
+            date={expenses[0].date}></ExpenseItem>
+        <ExpenseItem
+            title={expenses[1].title}
+            amount={expenses[1].amount}
+            date={expenses[1].date}></ExpenseItem>
+        <ExpenseItem
+            title={expenses[2].title}
+            amount={expenses[2].amount}
+            date={expenses[2].date}></ExpenseItem>
     </div>
   );
 }
