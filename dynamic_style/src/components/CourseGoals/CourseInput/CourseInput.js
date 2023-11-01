52480268c20f387f1c07c3074b548d8dd7199a72
Jaehoon Park <wido1593@gmail.com>
2023-11-01 22:04:34 +0900

    React.js 학습기록 - Styled Components 를 통해 동적으로 스타일 지정하기.

--- dynamic_style/src/components/CourseGoals/CourseInput/CourseInput.js
+++ dynamic_style/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,7 +3,40 @@ import React, { useState } from 'react';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
+import styled from "styled-components";
+
+//JSX 로 이용할 것이기에 이름은 대문자로 시작.
+// 내부 div에서만 지정될 스타일을 동적으로 지정.
+const FormControl = styled.div`
+      margin: 0.5rem 0;
+
+    & label {
+      font-weight: bold;
+      display: block;
+      margin-bottom: 0.5rem;
+      color:  ${props => props.invalid ? 'red' : 'black'};
+    }
+
+    & input {
+      display: block;
+      width: 100%;
+      border: 1px solid ${props=> props.invalid ? 'red' : '#ccc' };
+      background: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
+      font: inherit;
+      line-height: 1.5rem;
+      padding: 0 0.25rem;
+    }
+
+    & input:focus {
+      outline: none;
+      background: #fad0ec;
+      border-color: #8b005d;
+    }
+  `;
+
 const CourseInput = props => {
+
+
   const [enteredValue, setEnteredValue] = useState('');
 
   const [isValid, setIsValid] = useState(true); // 입력 상태 체크 State
@@ -28,13 +61,14 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
-        <input style={
-          {borderColor: !isValid ? 'red' : 'black',
-          background: !isValid ? 'salmon' : 'transparent'}
-        } type="text" onChange={goalInputChangeHandler} />
-      </div>
+      {/*컴포넌트 내부에서만 적용될 스타일 지정 태그 호출.
+      호출 시에 invalid을 동적으로 넘겨줘 styled components 내에서 ${} 을 이용해 조건을 설정할 수 있다.*/}
+      <FormControl invalid={!isValid}>
+      {/*<div className={`form-control ${!isValid ? 'invalid' : ''}`}>*/}
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      {/*</div>*/}
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
--- dynamic_style/src/components/UI/Button/Button.js
+++ dynamic_style/src/components/UI/Button/Button.js
@@ -1,13 +1,45 @@
 import React from 'react';
 
-import './Button.css';
+import styled from "styled-components";
+// import './Button.css';
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+//.button 메소드에 스타일을 전달하면 이를 토대로 새로운 버튼을 동적으로 반환한다.
+// 클래스마다 고유한 이름을 가지게 하기에 내부적으로만 사용하기 위한 방법이다.
+const Button = styled.button`
+    /*className을 설정할 위치가 없기에 클래스 이름을 설정하지 않아도
+    button에 직접적으로 영향을 준다.*/
+    width: 100%;
+    font: inherit;
+    padding: 0.5rem 1.5rem;
+    border: 1px solid #8b005d;
+    color: white;
+    background: #8b005d;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+    cursor: pointer;
+  
+  @media (min-width: 768px){
+    width: auto;
+  }
+  /*& 는 생성한 이 컴포넌트에 대해 특별한 가상선택자를 사용하겠다고 패키지에 선언.
+  이 버튼에 focus가 있으면 스타일을 적용해줘라고 선언.*/
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
+
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
