97b7a72893b0e09e6b905a6a66ad41f6624a177b
Jaehoon Park <wido1593@gmail.com>
2023-11-03 15:18:40 +0900

    React.js 학습기록 - ref를 이용한 제어되지 않는 컴포넌트 핸들링.

--- add_user/src/components/Users/AddUser.js
+++ add_user/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import Card from "../UI/Card";
 import styles from  './AddUser.module.css';
 import Button from "../UI/Button";
@@ -6,16 +6,20 @@ import ErrorModal from "../UI/ErrorModal";
 
 import Wrapper from "../Helper/Wrapper";
 const AddUser = props => {
+    /*ref 사용을 위한 HOOK 선언*/
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+    
     const initState = {
         'username': '',
         'age': ''
     }
 
-    const [userData, setUserData] = useState(initState);
+    /*const [userData, setUserData] = useState(initState);*/
 
     const [error, setError] = useState()
 
-    const inputChangeHandler = (input, value) => {
+    /*const inputChangeHandler = (input, value) => {
         setUserData((prevState) => {
             return{
                 ...prevState,
@@ -23,12 +27,15 @@ const AddUser = props => {
                 id: Math.random().toString()
             }
         })
-    }
+    }*/
 
     const addUserHandler = event => {
         event.preventDefault();
 
-        if(userData['username'].trim().length === 0 || userData['age'].trim().length === 0)
+        //ref로 return받은 요소 핸들링.
+        //ref는 DOM의 요소를 retunr하며, current 라는 객체의 이름으로 가져온다.
+        if(nameInputRef.current.value.trim().length === 0 || ageInputRef.current.value.trim().length === 0)
+            /*if(userData['username'].trim().length === 0 || userData['age'].trim().length === 0)*/
         {
             setError({
                 title: "Invalid input",
@@ -36,7 +43,8 @@ const AddUser = props => {
             });
             return;
         }
-        if(+userData['age'] < 1)
+        if(+ageInputRef < 1)
+            /*if(+userData['age'] < 1)*/
         {
             setError({
                 title: "Invalid age",
@@ -44,8 +52,16 @@ const AddUser = props => {
             });
             return;
         }
-        props.onAddUser(userData);
-        setUserData(initState);
+        /*props.onAddUser(userData);
+        setUserData(initState);*/
+        props.onAddUser(
+            {
+                'username' : nameInputRef.current.value,
+                'age' : ageInputRef.current.value
+            });
+        /*초기화*/
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
     }
 
     const errorHandler = () => {
@@ -62,19 +78,21 @@ const AddUser = props => {
                     <input
                         id={"username"}
                         type={"text"}
-                        value={userData['username']}
+                        /*value={userData['username']}
                         onChange={(event) => (
                             inputChangeHandler('username', event.target.value)
-                        )}
+                        )}*/
+                        ref={nameInputRef}//ref 속성 설정.
                     />
                     <label htmlFor={"age"}>Age</label>
                     <input
                         id={"age"}
                         type={"number"}
-                        value={userData['age']}
+                        /*value={userData['age']}
                         onChange={(event) => (
                             inputChangeHandler('age', event.target.value)
-                        )}/>
+                        )}*/
+                        ref={ageInputRef}/>
                     <Button type={"submit"}> Add User </Button>
                 </form>
             </Card>
