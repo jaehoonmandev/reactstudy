eb3dbb8e3dbfbf35d5cc94c188001ab026608f00
Jaehoon Park <wido1593@gmail.com>
2023-11-03 22:48:52 +0900

    React.js 학습기록 - 사용자 정의 컨텍스트 빌드

--- use_effect/src/App.js
+++ use_effect/src/App.js
@@ -1,43 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext} from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from "./store/auth-context";
 
 function App() {
-
-    // 의존성이 재평가 후 실행되는 리액트 Effect
-    useEffect(() => {
-        const storedUserLoggedInInformaion = localStorage.getItem('isLoggedIn');
-
-        if(storedUserLoggedInInformaion === '1'){
-            setIsLoggedIn(true);
-        }
-    },
-        // 의존성이 없을 때는 앱이 시작될 때만 실행된다. 즉, 한 번만 실행된다.
-        []);
-
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn', '1'); // 로컬 스토리지 사용하여 로그인 상태를 저장.
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.setItem('isLoggedIn', '0');
-    setIsLoggedIn(false);
-  };
+    const ctx = useContext(AuthContext);
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
+          <MainHeader />
+          <main>
+            {!ctx.isLoggedIn && <Login />}
+            {ctx.isLoggedIn && <Home />}
+          </main>
     </React.Fragment>
   );
 }
