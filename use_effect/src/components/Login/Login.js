abdf273d5c92f159d1ecee1ec1b2bd53af9a6e5a
Jaehoon Park <wido1593@gmail.com>
2023-11-03 17:22:32 +0900

    React.js 학습기록 - useEffect로 이벤트 리스닝.

---
+++ use_effect/src/components/Login/Login.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from 'react';
+
+import Card from '../UI/Card/Card';
+import classes from './Login.module.css';
+import Button from '../UI/Button/Button';
+
+const Login = (props) => {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [emailIsValid, setEmailIsValid] = useState();
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      setFormIsValid(
+          //최초 실행 시 실행
+          enteredEmail.includes("@") && enteredPassword.trim().length >6
+      );
+    }, 500);
+    //클린업 함수
+    return () => {
+      console.log('clean up');
+      // 타이핑을 할 때마다(의존하는 컴포넌트에 변화가 일어날 때마다) 타이머가 다시 설정된다.
+      // 함수가 실행되기 전에 항상 실행되기에 타이핑이 계속된다면 타이머를 재성정하고, 입력이 멈춘 후 동작을 수행한다.
+      clearTimeout(identifier);
+    };
+  }, [setFormIsValid, enteredEmail, enteredPassword]);
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+
+    // setFormIsValid(
+    //   event.target.value.includes('@') && enteredPassword.trim().length > 6
+    // );
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+
+    // setFormIsValid(
+    //   event.target.value.trim().length > 6 && enteredEmail.includes('@')
+    // );
+  };
+
+  const validateEmailHandler = () => {
+    setEmailIsValid(enteredEmail.includes('@'));
+  };
+
+  const validatePasswordHandler = () => {
+    setPasswordIsValid(enteredPassword.trim().length > 6);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(enteredEmail, enteredPassword);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${
+            emailIsValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${
+            passwordIsValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
