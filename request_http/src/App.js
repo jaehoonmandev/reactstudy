73bd52fad4baadad3e5a8e08bb26a682aa29adec
Jaehoon Park <wido1593@gmail.com>
2023-11-07 21:12:08 +0900

    React.js 학습기록 - HTTP 요청 > POST

--- request_http/src/App.js
+++ request_http/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -7,37 +7,72 @@ function App() {
 
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+
 
     // 비동기
-  async function fetchMovieHandler(){
-      setIsLoading(true);
-    const response = fetch('https://swapi.dev/api/films/')
-    const data = await (await response).json();
+  /*async function fetchMovieHandler(){*/
+    // useEffect가 무한루프에 빠지지 않기 위해  useCallback로 항상 동일한 함수 사용하기.
+  const fetchMovieHandler = useCallback(async ()=> {
 
-    const transformMovies = data.results.map(
-        movie => {
-            return {
-                id : movie.episode_id,
-                title: movie.title,
-                openingText: movie.opening_crawl,
-                releaseDate: movie.release_date
-            }
-        }
-    );
-    setMovies(transformMovies);
-      setIsLoading(false);
-  }
+          setIsLoading(true);
+          setError(null);
+          //try-catch 로 에러 핸들링.
+          try{
+              const response = fetch('https://swapi.dev/api/films/')
+              //정상 처리가 되지 않았다면 catch로 보내기
+              if(!(await response).ok){
+                  throw new Error('Something went wrong!');
+              }
+              const data = await (await response).json();
 
-  return (
+              const transformMovies = data.results.map(
+                  movie => {
+                      return {
+                          id : movie.episode_id,
+                          title: movie.title,
+                          openingText: movie.opening_crawl,
+                          releaseDate: movie.release_date
+                      }
+                  }
+              );
+              setMovies(transformMovies);
+          }catch(error){
+              setError(error.message)
+          }
+          setIsLoading(false);
+
+  }, []);
+
+    useEffect(() => {
+        fetchMovieHandler();
+    }, [fetchMovieHandler]); // 즉각적으로 요청을 보낸다.
+
+
+
+  let content = <p>Found no movies.</p>;
+
+    if(movies.length > 0){
+        content = <MoviesList movies={movies}/>;
+    }
+    if(movies.length === 0){
+        content = <p> Found no movies... </p>;
+    }
+    if(error){
+        content = <p>{error}</p>;
+    }
+    if(isLoading){
+        content = <p> Loading... </p>;
+    }
+
+    return (
     <React.Fragment>
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-          {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-          {!isLoading && movies.length === 0 && <p> Found no movies... </p>}
-
-          {isLoading && <p> Loading... </p>}
+          {content}
       </section>
     </React.Fragment>
   );
