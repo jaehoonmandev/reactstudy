553927189a33e0642915b436ec18552c2895064f
Jaehoon Park <wido1593@gmail.com>
2023-11-08 19:11:56 +0900

    React.js 학습기록 - 커스텀 훅을 이용한 form 검증 > useState => useReducer

--- form_and_user_input/src/hooks/use-input.js
+++ form_and_user_input/src/hooks/use-input.js
@@ -1,29 +1,64 @@
-import {useState} from "react";
+import {useReducer, useState} from "react";
+
+
+const intialInputState ={
+    value: '',
+    isTouched: false
+}
+
+const inputStateReducer = (state, action) => {
+
+    if(action.type === 'INPUT'){
+        return { value: action.value, isTouched: state.isTouched}
+    }
+    if(action.type === 'BLUR'){
+        return { isTouched: true, value: state.value}
+    }
+    if(action.type === 'RESET'){
+        return { isTouched: false, value: ''}
+    }
+
+    return inputStateReducer;
+}
 
 const useInput = (validateValue) => {
-    const [enteredValue, setEnteredValue] = useState('');
-    const [isTouched, setIsTouched] = useState(false);
+
+    const [inputState, dispatchInputState ] = useReducer(
+        inputStateReducer,
+        intialInputState);
+    // const [enteredValue, setEnteredValue] = useState('');
+    // const [isTouched, setIsTouched] = useState(false);
 
 
-    const isValid = validateValue(enteredValue);
-
-    const hasError = !isValid && isTouched;
+    const isValid = validateValue(inputState.value);
+    const hasError = !isValid && inputState.isTouched;
+    // const isValid = validateValue(enteredValue);
+    // const hasError = !isValid && isTouched;
 
     const valueChangeHandler = (event) => {
-        setEnteredValue(event.target.value);
+        dispatchInputState({
+            type: 'INPUT', value: event.target.value
+        })
+        // setEnteredValue(event.target.value);
     }
 
     const inputBlurHandler = () => {
-        setIsTouched(true);
+        dispatchInputState({
+            type: 'BLUR'
+        })
+        // setIsTouched(true);
     }
 
     const reset = ( ) => {
-        setEnteredValue('')
-        setIsTouched(false)
+        dispatchInputState({
+            type: 'RESET'
+        })
+        // setEnteredValue('')
+        // setIsTouched(false)
     }
 
     return{
-        value : enteredValue,
+        value : inputState.value,
         isValid,
         hasError,
         valueChangeHandler,
