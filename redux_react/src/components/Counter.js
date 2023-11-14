40956449ca750057a29ce247335db5cca0794457
Jaehoon Park <wido1593@gmail.com>
2023-11-14 20:15:33 +0900

    React.js 학습기록 - 리액트 리덕스 > 여러 Slice 관리하기

--- redux_react/src/components/Counter.js
+++ redux_react/src/components/Counter.js
@@ -1,5 +1,7 @@
 import classes from './Counter.module.css';
 import { useSelector, useDispatch } from 'react-redux'
+
+import { counterActions} from "../store/index";
 import INCREMENT from '../store/index'
 const Counter = () => {
     //액션을 전송할 Dispath 함수
@@ -8,31 +10,26 @@ const Counter = () => {
     //redux에 의해 관리되는 함수.
     //내부 함수는 react-redux가 실행한다.
     // 실행되면서 자동으로 구독과 해지가 설정된다.
-    const counter = useSelector(state => state.counter)
-    const show = useSelector(state => state.showCounter)
+    //const counter = useSelector(state => state.counter)
+    const counter = useSelector(state => state.counter.counter) //슬라이스 분할로 지정한 식별자 때문에 다시 지정해줘야함.
+    const show = useSelector(state => state.counter.showCounter)
 
     const incrementHandler = () => {
-        dispatch({
-            type: INCREMENT
-        })
+        dispatch(counterActions.increment())
     }
     const increseHandler = () => {
-        dispatch({
-            type: 'increse',
-            amount: 5
-
-        })
+        // dispatch({
+        //     type: 'increse',
+        //     amount: 5
+        // })
+        dispatch(counterActions.increase(5)); // 넘겨지는 값은 기본적으로 payload로 지정되기에 필드 이름을 정해서 보낼 수 없다.
     }
 
     const decrementHandler = () => {
-        dispatch({
-            type: 'decrement'
-        })
+        dispatch(counterActions.decrement())
     }
   const toggleCounterHandler = () => {
-      dispatch({
-          type: 'toggle'
-      })
+      dispatch(counterActions.toggleCounter())
   };
 
   return (
--- redux_react/src/store/index.js
+++ redux_react/src/store/index.js
@@ -1,18 +1,16 @@
 import { createStore } from 'redux';
 import {createSlice, configureStore} from '@reduxjs/toolkit'
 
-const initialState = {
+const initialCounterState = {
     counter: 0,
     showCounter:true
 }
-
 const INCREMENT = 'increment'
-
 
 //전역 상태의 Slice 만들어두기.(직접적인 관계가 아닌 여러 상태로 나눠졌다면.)
 const counterSlice = createSlice({
     name: 'counter',
-    initialState,
+    initialState: initialCounterState,
     reducers: { // 객체 혹은 맵을 지정한다. 이는 자동으로 리덕스에 의해 호출되어 최근 값을 받아온다.
         /*if문을 사용하지 않아도 어떤 액션에 따라 메서드가 자동으로 호출괸다.*/
         increment(state) {
@@ -22,7 +20,7 @@ const counterSlice = createSlice({
             state.counter--;
         },
         increase(state, action) {
-            state.counter = state.counter + action.amount
+            state.counter = state.counter + action.payload //디폴트 인식자 이름.
         },
         toggleCounter(state) {
             state.showCounter = !state.showCounter
@@ -31,46 +29,35 @@ const counterSlice = createSlice({
     }
 
 })
-//
-// const conterReducer = (state = initialState, action) => {
-//     if(action.type === INCREMENT){
-//         return{
-//             //state.count++ 는 redux에서 작업할 때는 절대 state를 변형하면 안된다.
-//             counter: state.counter + 1,
-//             showCounter: state.showCounter
-//         }
-//     }
-//
-//     if(action.type === 'increse'){
-//         return{
-//             counter: state.counter + action.amount,
-//             showCounter: state.showCounter
-//         }
-//     }
-//
-//     if(action.type === 'decrement'){
-//         return{
-//             counter: state.counter - 1,
-//             showCounter: state.showCounter
-//         }
-//     }
-//
-//     if(action.type === 'toggle'){
-//         return{
-//             counter: state.counter,
-//             showCounter: !state.showCounter
-//         }
-//     }
-//     return state;
-// }
 
-//const store = createStore(conterReducer);
-//const store = createStore(counterSlice.reducer);
-const store = configureStore({
-    reducer:counterSlice.reducer
-    // reducer: {
-    //     counter : counterSlice.reducer // 여러개 일때는 각각 키를 설정하여 병합할 수 있다.
-    // }
+
+//역할에 따른 슬라이스 분리
+
+const initialAuthState = {
+    isAuthenticated : false
+}
+const authSlice = createSlice({
+    name: 'authentication',
+    initialState: initialAuthState,
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true;
+        },
+        logout(state) {
+            state.isAuthenticated = false;
+        }
+    }
 })
+
+const store = configureStore({
+    //reducer:counterSlice.reducer
+    reducer: {
+        counter : counterSlice.reducer, // 여러개 일때는 각각 키를 설정하여 병합할 수 있다.
+        auth : authSlice.reducer
+    }
+})
+
+export const counterActions = counterSlice.actions; // 액션 객체 export
+export const authActions = authSlice.actions;
 
 export default store;
