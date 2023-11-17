d83fe3397f6e53e9944c682eb0f2bcbf3c747ca7
Jaehoon Park <wido1593@gmail.com>
2023-11-17 18:18:59 +0900

    React.js 학습기록 - Tanstatck 쿼리(리액트 쿼리) # optimistic update

--- tanstack/src/components/Events/EditEvent.jsx
+++ tanstack/src/components/Events/EditEvent.jsx
@@ -3,7 +3,7 @@ import {Link, useNavigate, useParams} from 'react-router-dom';
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import {useMutation, useQuery} from "@tanstack/react-query";
-import {fetchEvent, updateEvent} from "../../util/http.js";
+import {fetchEvent, queryClient, updateEvent} from "../../util/http.js";
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
@@ -19,6 +19,25 @@ export default function EditEvent() {
 
     const {mutate} = useMutation({
         mutationFn:updateEvent,
+        onMutate: async (data) => {//mutate를 실행하자마자 실행되며, mutate 실행시 넘긴 값이 넘어온다.
+            //쿼리에 의해 캐시된 데이터를 변경하라한다.
+            const newEvent = data.event;
+
+            await queryClient.cancelQueries({queryKey: ['events', param.id]}) //해당 아이디 쿼리 취소하여 이전 데이터 가져오지 않도록
+            const previousEvent =  queryClient.getQueriesData(['events', param.id]); // 이전 데이터를 가져온다
+            // 응답을 기다리지 않고 직접 저장된 데이터를 수정한다.
+            queryClient.setQueriesData(['events', param.id], newEvent);
+
+            return {previousEvent};// 롤백하는 데이터를 onError에 넘길 수 있도록.
+        },
+        onError: (error,data, context) => {
+            //에러가 발생했을 때 이전 값을 가져와 다시 롤백해야한다.
+            queryClient.setQueryData(['events', param.id], context.previousEvent)
+        },
+        onSettled: () => { // mutation 동작이 완료될 때 마다 실행.
+            //동기화 되지 않을 때 쿼리에 데이터를 내부적으로 다시 가져와 다시 동기화시킨다.
+            queryClient.invalidateQueries(['events', param.id])
+        },
     })
 
     function handleSubmit(formData) {
